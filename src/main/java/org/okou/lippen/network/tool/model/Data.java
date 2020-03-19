47d0ef49a5ca8c39ee986a3b283b83e4ca8cf1fe
Mrmo2019 <56347291+Mrmo2019@users.noreply.github.com>
2020-03-19 19:45:43 +0800

    2020.3.19

---
+++ src/main/java/org/okou/lippen/network/tool/model/Data.java
@@ -0,0 +1,32 @@
+package org.okou.lippen.network.tool.model;
+
+import org.okou.lippen.network.tool.util.DataFormatUtil;
+
+public class Data {
+    private final byte[] data;
+    private final DataManager manager;
+
+    public Data(byte[] data, DataManager manager) {
+        super();
+        this.data = data;
+        this.manager = manager;
+    }
+
+    @Override
+    public String toString() {
+        String result = null;
+        switch (manager.getReadType()) {
+            case HEX:
+                result = toHexString();
+                break;
+            case STRING:
+                result = new String(data, manager.getCharset());
+                break;
+        }
+        return result;
+    }
+
+    private String toHexString() {
+        return DataFormatUtil.bytesToHex(data);
+    }
+}
